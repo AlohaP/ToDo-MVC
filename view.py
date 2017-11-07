af0fe7057c1580fc3d44b380e778aa56e0523f29
Piotr Górski <piotrek.gorski90@gmail.com>
2017-11-07 23:57:51 +0100

    Add view module with few basic functions

---
+++ view.py
@@ -0,0 +1,23 @@
+class View:
+
+    @staticmethod
+    def display_menu():
+        print('1. Add ToDo item \n', '2. Modify item\n', '3. Delete item \n', '4. Mark item\n',
+              '5. Display item details\n', '6. Display item list\n', '7. Exit'
+              )
+
+    @staticmethod
+    def display_item_add_result():
+        print('Item added')
+
+    @staticmethod
+    def display_item_delete():
+        print('Item deleted')
+
+    @staticmethod
+    def display_item_mark():
+        print('Item marked')
+
+    @staticmethod
+    def display_wrong_input():
+        print('Wrong input')

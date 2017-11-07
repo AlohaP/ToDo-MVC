d3362cd3bc0618a7acaad77b0d3b3df097776490
Piotr Górski <piotrek.gorski90@gmail.com>
2017-11-08 00:07:53 +0100

    Added controler to get user input for new task, need to work on init

---
+++ item_controller.py
@@ -0,0 +1,15 @@
+from model_todo_item import ToDoItem
+from view import View
+
+class ItemController():
+
+    def __init__(self):
+        pass
+
+    def create_item(self):
+        item_name = View.user_input('Type item name: ')
+        item_description = View.user_input('\nType short description (max 150 characters):')
+        try:
+            return ToDoItem(item_name, item_description)
+        except AttributeError:
+            View.display_wrong_input()

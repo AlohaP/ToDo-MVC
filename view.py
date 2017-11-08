c88d934d5b062e4ba8ef124f2e8daa6012c8f21a
Piotr Górski <piotrek.gorski90@gmail.com>
2017-11-08 02:39:50 +0100

    add display options for detailed item, replacing instruction and user input

--- view.py
+++ view.py
@@ -1,13 +1,17 @@
 class View:
 
     @staticmethod
+    def user_input(message):
+        return input(message)
+
+    @staticmethod
     def display_menu():
-        print('1. Add ToDo item \n', '2. Modify item\n', '3. Delete item \n', '4. Mark item\n',
-              '5. Display item details\n', '6. Display item list\n', '7. Exit'
+        print(' (1) Add ToDo item \n', '(2) Modify item\n', '(3) Delete item \n', '(4) Mark item\n',
+              '(5) Display item details\n', '(6) Display item list\n', '(7) Exit'
               )
 
     @staticmethod
-    def display_item_add_result():
+    def display_item_add():
         print('Item added')
 
     @staticmethod
@@ -26,3 +30,11 @@ class View:
     def display_items(item_list):
         for id, item in enumerate(item_list):
             print(id, ' ' + item)
+
+    @staticmethod
+    def display_detailed_item(item):
+        print(item)
+
+    @staticmethod
+    def replace_instruction():
+        print('>>>First enter new name and description then choose wich item you want to modify<<<')
